2b12ef6ed93d5b28c48d74d1c59d09e08b22c9d8
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-03-17 11:13:27 +0100

    Kezdetek

---
+++ jq.js
@@ -0,0 +1,27 @@
+const listaRM = [{
+    nev : "Lajos",
+    tul1 : "éhes",
+    tul2 : "nagyon éhes",
+},{
+    nev : "Csaba",
+    tul1 : "éhes",
+    tul2 : "nagyon éhes",
+},{
+    nev : "Gáspárlajos",
+    tul1 : "éhes",
+    tul2 : "nagyon éhes",
+}
+]
+$(function () {
+    const TABEL = $("table");
+    let kiíras = osszeallit();
+    TABEL.append(kiíras);
+})
+
+function osszeallit() {
+    let txt =  `<tr><th>nev</th><td>tul1</td><td>tul2</td></tr>`;
+    for (let i = 0; i < listaRM.length; i++) {
+        txt += `<tr><th>${listaRM[i].nev}</th><td>${listaRM[i].tul1}</td><td>${listaRM[i].tul2}</td></tr>`
+    }
+    return txt
+}
